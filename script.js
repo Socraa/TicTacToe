3e1fd4bef2f9823d373ad1539c412ce773549440
Jai Dyelle Dela Cruz <Jaidyelle12@gmail.com>
2024-06-24 21:26:32 +0800

    incase of emergency

---
+++ script.js
@@ -0,0 +1,9 @@
+const cells = document.querySelectorAll('.cell');
+const status = document.querySelector('#statusText');
+const restart = document.querySelector('#restartBtn');
+
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8]
+]
